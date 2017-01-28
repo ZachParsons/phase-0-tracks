e9a46d8c11323bb4ac1c74824f4c5e1b032ad0f2
“ZachParsons” <zzttpp@gmail.com>
2017-01-28 02:03:33 -0600

    complete gps3.1

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -3,27 +3,33 @@
 # I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
 
+# Release 1: require_relative
 # EXPLANATION OF require_relative
 #
-#
+# require_relative allows for loading/accessing another file via a relative path to the same local directory, its signature returns true-false
+# in contrast, require allows for loading/accessing another file from an absolute path to even remote directories such as code libraries or gems
 require_relative 'state_data'
 
 class VirusPredictor
-
+# assigns attributes to instance variables by taking takes three arguments
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+# a wrapper method that returns the output/results of two other methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
+# Release 6: Private
+# private is a built-in method (in docs under module) that sets visibility of subsequently defined methods
+# if placed above method virus_effects, program breaks
   private
-
-  def predicted_deaths(population_density, population, state)
+  # conditionals setting ranges of population density, which calculates disease spread, assigns total deaths, prints string saying so in English
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,20 +47,22 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# conditionals setting ranges of population density, which determines speed of disease spread
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
+    case
+    when @population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
+    when @population_density >= 150
       speed += 1
-    elsif @population_density >= 100
+    when @population_density >= 100
       speed += 1.5
-    elsif @population_density >= 50
+    when @population_density >= 50
       speed += 2
-    else
+    when
       speed += 2.5
     end
 
@@ -69,19 +77,41 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+
+# Release 4: Implement a new feature
+STATE_DATA.each do |state_name, properties|
+  # properties.each do |key, population|
+  state_projection = VirusPredictor.new(state_name, properties[:population_density], properties[:population])
+  state_projection.virus_effects
+end
 
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# In the state data file, the nested hashes have this structure: h = {k => v{k;v, k:v}}, i.e. the deepest two key-value pairs are using symbol notation & are themselves the value of the state_name key using hash rocket notation. 
+
+# # What does require_relative do? How is it different from require?
+# (from above line 9) require_relative allows for loading/accessing another file via a relative path to the same local directory, its signature returns true-false, in contrast, require allows for loading/accessing another file from an absolute path to even remote directories such as code libraries or gems
+
+# # What are some ways to iterate through a hash?
+# Iterating through a hash can be done with block method do and built-in methods .each, & .map.
+
+# # When refactoring virus_effects, what stood out to you about the variables, if anything?
+# It was interesting that only the method initalize needed to take explicit arguments.
+
+# # What concept did you most solidify in this challenge?
+# The concepts solidied in this challenge were using *class methods* to iterate through & process a nested hash.
