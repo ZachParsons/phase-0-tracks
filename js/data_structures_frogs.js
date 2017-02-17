5d3170992889803af5399c5582ea9a2ae1c05dce
“ZachParsons” <zzttpp@gmail.com>
2017-02-17 15:56:53 -0600

    Initial commit

---
+++ js/data_structures_frogs.js
@@ -0,0 +1,12 @@
+// 7.2 Mandatory Pairing: Data Structures
+// Release 0: Work with Arrays
+
+
+// Release 1: Build an Object
+
+
+// Release 2: Build Many Objects Using a Constructor
+
+
+// Release 3: Research on Your Own
+
